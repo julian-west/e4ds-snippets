c21da6f404e81a6dd925d53dcbc26ab292c21afb
julian-west <westjc@btopenworld.com>
2021-10-23 15:00:41 +0100

    refactor to use object oriented design for input section

--- jupyter/voila/part1/content.py
+++ jupyter/voila/part1/content.py
@@ -8,12 +8,15 @@ import seaborn as sns
 class ContentSection:
     """Defined section of content"""
 
-    def __init__(self, stock_prices_df: pd.DataFrame, name: str, description: str):
-        self.stock_prices_df = stock_prices_df
+    def __init__(self, name: str, description: str, stock_prices: pd.DataFrame):
         self.name = name
         self.description = description
+        self.stock_prices = stock_prices
         self.html_description = self._render_html_description()
         self.content = self.build()
+
+    def _render_html_description(self):
+        return widgets.HTML(value=(f"<p><strong>{self.description}</strong></p>"))
 
     def build(self):
         """Build the tab"""
@@ -23,72 +26,57 @@ class ContentSection:
         """Action to update the tab"""
         raise NotImplementedError
 
-    def _render_html_description(self):
-        return widgets.HTML(value=(f"<p><strong>{self.description}</strong></p>"))
-
 
 class RebasedStockGraph(ContentSection):
-    def __init__(self, stock_prices_df: pd.DataFrame, name: str, description: str):
-        super().__init__(stock_prices_df, name, description)
+    def __init__(self, name: str, description: str, stock_prices: pd.DataFrame):
+        super().__init__(name, description, stock_prices)
 
     def build(self) -> widgets.widgets.widget_box.VBox:
         self.output = widgets.Output()
         with self.output:
-            self.stock_prices_df.rebase().plot(figsize=(12, 5))
+            self.stock_prices.rebase().plot(figsize=(12, 5))
             plt.title("Share price performance (rebased)")
             plt.show()
 
         return widgets.VBox([self.output])
 
-    def update(self):
-        pass
-
 
 class StockStatistics(ContentSection):
-    def __init__(self, stock_prices_df: pd.DataFrame, name: str, description: str):
-        super().__init__(stock_prices_df, name, description)
+    def __init__(self, name: str, description: str, stock_prices: pd.DataFrame):
+        super().__init__(name, description, stock_prices)
 
     def build(self) -> widgets.widgets.widget_box.VBox:
         self.output = widgets.Output()
         with self.output:
-            self.stock_prices_df.calc_stats().display()
+            self.stock_prices.calc_stats().display()
 
         return widgets.VBox([self.html_description, self.output])
 
-    def update(self):
-        pass
-
 
 class Correlations(ContentSection):
-    def __init__(self, stock_prices_df: pd.DataFrame, name: str, description: str):
-        super().__init__(stock_prices_df, name, description)
+    def __init__(self, name: str, description: str, stock_prices: pd.DataFrame):
+        super().__init__(name, description, stock_prices)
 
     def build(self) -> widgets.widgets.widget_box.VBox:
         self.output = widgets.Output()
         with self.output:
-            returns = self.stock_prices_df.to_log_returns().dropna()
+            returns = self.stock_prices.to_log_returns().dropna()
             correlations = returns.corr()
             sns.heatmap(correlations, fmt=".2f", cmap="Blues", annot=True, cbar=False)
             plt.show()
 
         return widgets.VBox([self.html_description, self.output])
 
-    def update(self):
-        pass
-
 
 class Drawdown(ContentSection):
-    def __init__(self, stock_prices_df: pd.DataFrame, name: str, description: str):
-        super().__init__(stock_prices_df, name, description)
+    def __init__(self, name: str, description: str, stock_prices: pd.DataFrame):
+        super().__init__(name, description, stock_prices)
 
     def build(self) -> widgets.widgets.widget_box.VBox:
         self.output = widgets.Output()
         with self.output:
-            self.stock_prices_df.to_drawdown_series().plot()
+            self.stock_prices.to_drawdown_series().plot()
             plt.title("Drawdown Plot")
             plt.show()
 
         return widgets.VBox([self.html_description, self.output])
-
-    def update(self):
-        pass
--- jupyter/voila/part1/gui.py
+++ jupyter/voila/part1/gui.py
@@ -3,13 +3,25 @@
 import ipywidgets as widgets
 from content import ContentSection
 from IPython.display import display
+from ipywidgets.widgets.widget_button import Button
+from user_input import UserInputWidget
 
 
 class Gui:
     """Responsible for building the entire user interface"""
 
-    def __init__(self, main: ContentSection, tabs: list[ContentSection]):
-        self.user_input_area = None
+    def __init__(
+        self,
+        user_inputs: list[UserInputWidget],
+        action_button: Button,
+        main: ContentSection,
+        tabs: list[ContentSection],
+    ):
+
+        self.user_inputs = {
+            user_input.name: user_input.widget for user_input in user_inputs
+        }
+        self.action_button = action_button
         self.main = main
         self.tabs = tabs
 
@@ -24,4 +36,12 @@ class Gui:
         for i, tab in enumerate(self.tabs):
             self.tab_container.set_title(i, tab.name)
 
+        display(
+            widgets.HBox(
+                [
+                    widgets.VBox([widget for widget in self.user_inputs.values()]),
+                    self.action_button,
+                ]
+            )
+        )
         display(self.main.content, self.tab_container)
